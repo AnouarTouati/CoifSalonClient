e2aecd718677816279a251bc96846fc7bc3c4f94
KasperMA <marouan1996@hotmail.fr>
2019-08-05 19:41:35 +0100

    V1

---
+++ app/src/main/java/com/example/coifsalonclient/ShopDetails_Frag3.java
@@ -0,0 +1,41 @@
+package com.example.coifsalonclient;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class ShopDetails_Frag3 extends Fragment {
+   static  CustomRecyclerViewAdapterFrag3Reviews customRecyclerViewAdapterFrag3Reviews;
+   static  RecyclerView recyclerView;
+    public static Context mContext;
+    View view;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mContext=getContext();
+        view=inflater.inflate(R.layout.shopdetails_frag3, container, false);
+        recyclerView=view.findViewById(R.id.recyclerView_Frag3);
+        customRecyclerViewAdapterFrag3Reviews=new CustomRecyclerViewAdapterFrag3Reviews(ShopDetailsActivity.reviewersNames, ShopDetailsActivity.reviewersComments, ShopDetailsActivity.reviewersCommentDate, ShopDetailsActivity.reviewersGivenStars, getContext());
+        recyclerView.setAdapter(customRecyclerViewAdapterFrag3Reviews);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        ///////////////////////////////////////////////////////////
+        ///the code below is used after recyclerView is initialized so we dont get null reference
+        if(ShopDetailsActivity.reviewersNames.size()==0){
+            ShopDetailsActivity.GetStoreReviewsInfo(ShopDetailsActivity.ShopNameFromRecyclerView);
+        }
+
+        return view;
+    }
+    public static void ReceivedReviewersInfoNotifyRecyclerViewAdapter(){
+        customRecyclerViewAdapterFrag3Reviews=new CustomRecyclerViewAdapterFrag3Reviews(ShopDetailsActivity.reviewersNames, ShopDetailsActivity.reviewersComments, ShopDetailsActivity.reviewersCommentDate, ShopDetailsActivity.reviewersGivenStars, mContext);
+        recyclerView.swapAdapter(customRecyclerViewAdapterFrag3Reviews, true);
+    }
+}
