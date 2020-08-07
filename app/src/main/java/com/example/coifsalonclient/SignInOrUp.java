c194b213d06bf09e6ff8342ad1debe233d7b90ec
Anouar Touati <41107778+KasperMA@users.noreply.github.com>
2020-08-07 23:19:16 +0100

    V14

--- app/src/main/java/com/example/coifsalonclient/SignInOrUp.java
+++ app/src/main/java/com/example/coifsalonclient/SignInOrUp.java
@@ -10,17 +10,22 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignInOrUp extends AppCompatActivity {
 
     FirebaseAuth firebaseAuth;
-    EditText emailEditTex, passwordEditText;
+    FirebaseUser firebaseUser;
+    EditText emailEditTex, passwordEditText, nameEditText;
+    Button signInButton, signUpButton, submitNameButton;
+    TextView errorTextView;
     Context mContext;
 
     @Override
@@ -30,22 +35,33 @@ public class SignInOrUp extends AppCompatActivity {
 
         mContext = this;
         firebaseAuth = FirebaseAuth.getInstance();
-         emailEditTex=findViewById(R.id.editTextTextEmailAddress);
-         passwordEditText=findViewById(R.id.editTextTextPassword);
-        Button signInButton = findViewById(R.id.signInButton);
+
+        emailEditTex = findViewById(R.id.editTextTextEmailAddress);
+        passwordEditText = findViewById(R.id.editTextTextPassword);
+        signInButton = findViewById(R.id.signInButton);
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 signIn();
             }
         });
-    Button  signUpButton=findViewById(R.id.signUp);
-    signUpButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            signUp();
-        }
-    });
+        signUpButton = findViewById(R.id.signUp);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signUp();
+            }
+        });
+        nameEditText = findViewById(R.id.nameEditText);
+        submitNameButton = findViewById(R.id.submitNameButton);
+        submitNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTheName();
+            }
+        });
+        errorTextView = findViewById(R.id.errorTextView);
+        errorTextView.setVisibility(View.GONE);
     }
 
 
@@ -70,8 +86,8 @@ public class SignInOrUp extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                     @Override
                     public void onSuccess(AuthResult authResult) {
-                        Intent goToMainActivity = new Intent(mContext, MainActivity.class);
-                        startActivity(goToMainActivity);
+                        firebaseUser = authResult.getUser();
+                        showNameField();
                     }
                 }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -79,5 +95,47 @@ public class SignInOrUp extends AppCompatActivity {
                 Log.v("MyFirebase", "Failed to sign up");
             }
         });
+
+    }
+
+    private void showNameField() {
+        ////////////////////////////////////////
+        emailEditTex.setVisibility(View.GONE);
+        passwordEditText.setVisibility(View.GONE);
+        signInButton.setVisibility(View.GONE);
+        signUpButton.setVisibility(View.GONE);
+        ///////////////////////////////////////
+        nameEditText.setVisibility(View.VISIBLE);
+        submitNameButton.setVisibility(View.VISIBLE);
+        errorTextView.setVisibility(View.GONE);
+    }
+
+    private void setTheName() {
+        if (firebaseUser != null) {
+            errorTextView.setVisibility(View.GONE);
+            String name = nameEditText.getText().toString();
+            if (name.length() > 3) {
+                UserProfileChangeRequest userProfileChangeRequest = new UserProfileChangeRequest.Builder()
+                        .setDisplayName(name).build();
+                firebaseUser.updateProfile(userProfileChangeRequest).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Intent goToMainActivity = new Intent(mContext, MainActivity.class);
+                        startActivity(goToMainActivity);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.v("MyFirebase", "Failed to sign up");
+                    }
+                });
+            } else {
+                errorTextView.setText(R.string.invalid_name);
+                errorTextView.setVisibility(View.VISIBLE);
+            }
+
+        }
+
+
     }
 }
